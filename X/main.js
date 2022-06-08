e37b0e1f5ba590f27490ff54d479f7a315b6ed5b
Yujin Jeon <miru93198@gmail.com>
2022-06-09 02:52:54 +0900

    오늘만 세번째

--- X/main.js
+++
@@ -1,9 +0,0 @@
-const toggle = document.querySelector('.navbar__toggle');
-const menu = document.querySelector('.dropdown');
-const content = document.querySelector('.dropdown_menu');
-
-toggle.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    content.classList.toggle('active');
-    //icons.classList.toggle('active');
-})
